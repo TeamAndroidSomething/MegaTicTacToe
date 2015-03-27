e448ec190dd607bca13417ec145f244e0f6b83af
YuanChen <rosmarinuscyn@gmail.com>
2015-03-26 21:29:54 -0400

    Done~~ rename all the button, include those board

--- CapstoneApp/src/main/java/com/example/megatictactoe/megatictactoe/GameActivity.java
+++ CapstoneApp/src/main/java/com/example/megatictactoe/megatictactoe/GameActivity.java
@@ -15,6 +15,7 @@ public class GameActivity extends Activity {
     }
 
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
